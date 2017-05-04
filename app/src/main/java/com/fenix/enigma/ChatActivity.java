98a8af25d08f92d5d25dd26f96401e8de9392a87
Fran Jurinec <franjurinec@live.com>
2017-05-04 17:41:46 +0200

    UI Changes

--- app/src/main/java/com/fenix/enigma/ChatActivity.java
+++ app/src/main/java/com/fenix/enigma/ChatActivity.java
@@ -7,13 +7,10 @@ import android.view.View;
 
 public class ChatActivity extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
