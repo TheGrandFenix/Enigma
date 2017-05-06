bea1606f479068e276a4996c28603950ca415fa6
Fran Jurinec <franjurinec@live.com>
2017-05-06 11:14:04 +0200

    Locked rotation, improved logout

--- app/src/main/java/com/fenix/enigma/activities/ChatActivity.java
+++ app/src/main/java/com/fenix/enigma/activities/ChatActivity.java
@@ -3,8 +3,12 @@ package com.fenix.enigma.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fenix.enigma.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,16 +34,30 @@ public class ChatActivity extends Activity {
             startActivity(new Intent(this, LoginActivity.class));
         }
 
+        EditText messageInput = (EditText) findViewById(R.id.inputMessage);
+        messageInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    sendMessage(view);
+                    return true;
+                }
+                return false;
+            }
+        });
+
         //Test user preview [REMOVE]
         TextView userPreview = (TextView) findViewById(R.id.userPreview);
         if (user != null) userPreview.setText(user.getEmail());
     }
 
     public void sendMessage(View view) {
-
+        Toast.makeText(ChatActivity.this, "Stop, there is no chat.",
+                Toast.LENGTH_SHORT).show();
     }
 
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
+        recreate();
     }
 }
